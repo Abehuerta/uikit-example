1d409c2e4a3a1bdf51554e3563a2f0e32d6af8cd
Abraham Huerta <abrhuerta@gmail.com>
2021-12-30 16:05:11 -0600

    first commit

---
+++ .storybook/preview.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { withThemesProvider } from "themeprovider-storybook";
+import light from "../src/theme/light";
+import dark from "../src/theme/dark";
+import ResetCSS from "../src/ResetCSS";
+
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+const themes = [
+  {
+    name: "Light",
+    backgroundColor: light.colors.background,
+    ...light,
+  },
+  {
+    name: "Dark",
+    backgroundColor: dark.colors.background,
+    ...dark,
+  },
+];
+
+export const decorators = [globalDecorator, withThemesProvider(themes)];
